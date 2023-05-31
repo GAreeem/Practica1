d3af667abd7636bc4af9c4c58bf053c375d65938
Emilio <20223tn084@utez.edu.mx>
2023-05-31 11:28:58 -0600

    Update(31-05-2023)

--- firstapp/src/main/java/mx/edu/utez/firstapp/models/crud/DaoRepository.java
+++ firstapp/src/main/java/mx/edu/utez/firstapp/models/crud/DaoRepository.java
@@ -1,8 +1,10 @@
 package mx.edu.utez.firstapp.models.crud;
+import java.util.List;
 
 public interface DaoRepository<T>{
 List<T> findAll();
 T findOne(Long id);
+boolean save(T object);
 boolean update(T object);
 boolean delete(Long id);
 }
--- firstapp/src/main/java/mx/edu/utez/firstapp/models/user/DaoUser.java
+++ firstapp/src/main/java/mx/edu/utez/firstapp/models/user/DaoUser.java
@@ -1,9 +1,55 @@
 package mx.edu.utez.firstapp.models.user;
 import mx.edu.utez.firstapp.models.crud.DaoRepository;
+import mx.edu.utez.firstapp.utils.MySQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DaoUser implements DaoRepository<user>{
+    private Connection conn;
+    private PreparedStatement pstm;
+    private ResultSet rs;
+    @Override
+    public List<user> findAll() {
+        List<user> users = new ArrayList<>();
+        try{
+            conn= new MySQLConnection().connect();
+            String query ="SELECT * FROM users;";
+            pstm = conn.prepareStatement(query);
+            rs = pstm.executeQuery();
+            while (rs.next()){
+                user user = new user();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setSurname(rs.getString("surname"));
+                user.setLastname(rs.getString("lastname"));
+                user.setBirthday(rs.getString("birthday"));
+                user.setUsername(rs.getString("username"));
+                user.setStatus(rs.getString("status"));
+                users.add(user);
+            }
+        }catch (SQLException e){
+            Logger.getLogger(DaoUser.class.getName()).log(Level.SEVERE, "Error findAll "+ e.getMessage());
+        }finally {
+            close();
+        }
+        return users;
+    }
+
     @Override
     public user findOne(Long id) {
         return null;
+    }
+
+    @Override
+    public boolean save(user object) {
+        return false;
     }
 
     @Override
@@ -14,5 +60,15 @@ public class DaoUser implements DaoRepository<user>{
     @Override
     public boolean delete(Long id) {
         return false;
+    }
+
+    public void close(){
+        try{
+            if (conn != null) conn.close();
+            if (pstm != null) pstm.close();
+            if (rs != null) rs.close();
+        }catch (SQLException e){
+
+        }
     }
 }
